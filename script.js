d6ea10d7063b4661792d33a3720a26f0cfb9fe64
MikaelBhai <126885689+mikaelbhai@users.noreply.github.com>
2023-04-28 15:24:45 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+body {
+  background-color: #1d1d1d;
+}
+
+.calculator {
+  width: 300px;
+  margin: 50px auto;
+  background-color: #333333;
+  border-radius: 10px;
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.4);
+}
+
+.display {
+  height: 60px;
+  padding: 10px;
+  text-align: right;
+}
+
+.display p {
+  margin: 0;
+  font-size: 36px;
+  color: #ffffff;
+}
+
+.buttons {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+  padding: 10px;
+}
+
+button {
+  height: 50px;
+  border-radius: 5px;
+  border: none;
+  font-size: 24px;
+  font-weight: bold;
+  color: #ffffff;
+  background-color: #555555;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+}
+
+button:hover {
+  background-color: #777777;
+}
